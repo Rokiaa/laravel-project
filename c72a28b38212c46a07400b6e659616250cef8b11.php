4ff358f3ae1abf19564a5d447e0bad500605c792
rokiaa <44901810+Rokiaa@users.noreply.github.com>
2020-09-19 21:34:56 +0200

    Add files via upload

---
+++ c72a28b38212c46a07400b6e659616250cef8b11.php
@@ -0,0 +1,38 @@
+
+<?php $__env->startSection('content'); ?>
+<?php echo Form::open(['url'=>'posts','method'=>'post']); ?>
+
+<div class="form-group">
+<label for="title">Title</label>
+<?php echo Form::text('title','',['placeholder'=>' Title...','required']); ?>
+
+<?php $__errorArgs = ['title'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+<div class="alert alert-danger"><?php echo e($message); ?></div>
+<?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+</div>
+<div class="from-group">
+<label for="body">Body</label>
+<?php echo Form::text('body','',['placeholder'=>'Body... ']); ?>
+
+<?php $__errorArgs = ['body'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+<div class="alert alert-danger"><?php echo e($message); ?></div>
+<?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+</div>
+<button type="submit" class="btn btn-primary">submit</button>
+<?php echo Form::Close(); ?>  
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\project\laravel\summertrain\resources\views/posts/create.blade.php ENDPATH**/ ?>
---
+++ d46d6bde1c68451b7a02daf3b7c1931fd1062069.php
@@ -0,0 +1,34 @@
+
+<?php $__env->startSection('content'); ?>
+<center><a class="btn btn-success ml-5" href="<?php echo e(route('posts.create')); ?>" id="createNewItem"> New</a></center>
+<table border=2 align="center">
+<thead>
+<th scope="col">#</th>
+<th scope="col">Title</th>
+<th scope="col">Body</th>
+<th scope="col">User Id</th>
+<th scope="col">Edit</th>
+<th scope="col">Delete</th>
+<th scope="col">Show</th>
+</thead>
+<tbody>
+<?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+<tr>
+<th scope="row"><?php echo e($post->id); ?></th>
+<td><?php echo e($post->title); ?></td>
+<td><?php echo e($post->body); ?></td>
+<td><?php echo e($post->user_id); ?></td>
+<td><a href="<?php echo e(route('posts.edit',$post->id)); ?>">Edit </a></td>
+<td>
+<?php echo Form::open(['route'=>['posts.destroy',$post->id],'method'=>'delete']); ?>
+
+<button type="submit" class="btn btn-primary">Delete</button> 
+<?php echo Form::close(); ?></td>
+<td><a href="<?php echo e(route('posts.show',$post->id)); ?>">Show </a></td>
+</tr>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+</tbody>
+</table>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\project\laravel\summertrain\resources\views/posts/list.blade.php ENDPATH**/ ?>
